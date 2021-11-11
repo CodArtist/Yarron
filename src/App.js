5856e213c32dc7cdc3f0bea522e455a8eaafa7b3
CodArtist <jainh7038@gmail.com>
2021-11-11 16:47:06 +0530

    Updated Landing Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import woman from './assets/Woman.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Yarron</h1>
+        <img src={woman} id="woman"/>
+
       </header>
     </div>
   );
